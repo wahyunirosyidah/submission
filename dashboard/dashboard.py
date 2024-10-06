ff4fdcbfa35f2cb39fdce4a93a3a4bb4dc18a82c
Wahyuni Fajrin Rosyidah <wahyunirosyidah@gmail.com>
2024-10-06 21:27:06 +0900

    change requirements

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 import calendar
 sns.set(style='dark')
 
